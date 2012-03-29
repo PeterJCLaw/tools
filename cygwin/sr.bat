48b2ad692a3c299eed7520b73a798dc4e4f3f236
Peter Law <PeterJCLaw@gmail.com>
2012-03-29 21:40:26 +0100

    Add a work-in-progress attempt to port to cygwin + windows python.
    This should *not* be used in production yet, and includes a reasonable
    amount of debug stuff.
    
    The changes the sitepackage are mostly complete, but the changes in the
    wrapper are highly experimental. We probably need a better (global)
    way to detect that we're on cygwin, and then behave differently in
    a few key places.
    
    The files in the new cygwin folder should either be copied to somewhere
    that's on your $PATH, or just add that folder to $PATH.

---
+++ cygwin/sr.bat
@@ -0,0 +1,2 @@
+@echo off
+python %UserProfile%\.sr\tools\wrapper\sr %*
--- wrapper/sitepackage.py
+++ wrapper/sitepackage.py
@@ -17,7 +17,7 @@ def check_installed():
     if os.path.exists(sym):
         try:
             cur_target = os.readlink( sym )
-            c = os.path.abspath( cur_target )
+            c = os.path.abspath( sym )
 
             if c == target:
                 "Installed, and pointing at the right place"
@@ -26,9 +26,28 @@ def check_installed():
             # Wrong target directory -- rewrite it
             os.unlink( sym )
 
+        except AttributeError:
+            "Python is unable to validate symbolic links on this platform, so try an alternate method"
+            import tempfile
+            (h, tf) = tempfile.mkstemp( dir = target )
+            os.close(h)
+            tf = os.path.basename(tf)
+            if not os.path.exists(os.path.join( sym, tf )):
+                "It's not a link-ish thing, so fake up an error that will be caught below"
+                raise OSError
+            else:
+                "We've shown that it is link-ish"
+                return
+
         except OSError:
             print >>sys.stderr, "Error: %s is not a symlink.  Refusing to continue." % sym
             exit(1)
 
     print >>sys.stderr, "Installing SR python usersitepackage"
+
+    if not hasattr(os, 'symlink'):
+        print >>sys.stderr, "Error: Python is unable to control symbolic links on your platform"
+        print >>sys.stderr, "Please create a link from '%s' to '%s'" % ( target, sym )
+        exit(1)
+
     os.symlink( target, sym )
